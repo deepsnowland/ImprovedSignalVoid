136fd2322852c2f4214be628d7061fa63ea50cc0
Fuar11 <mcaa6556@gmail.com>
2023-07-23 17:39:25 -0400

    Fixed UI issues in 2.22

--- Patches/MissionPatches.cs
+++ Patches/MissionPatches.cs
@@ -12,254 +12,40 @@ using UnityEngine;
 using Il2CppParadoxNotion.Services;
 using Il2CppNodeCanvas.Tasks.Conditions;
 using ImprovedSignalVoid.GearSpawns;
+using static Il2Cpp.Panel_Log;
 
 namespace ImprovedSignalVoid.Patches.Patches
 {
     internal class MissionPatches : MonoBehaviour
     {
 
-        [HarmonyPatch(typeof(Panel_Log), nameof(Panel_Log.Update))]
+        
+        [HarmonyPatch(typeof(Il2CppTLD.UI.MiniTopNav), nameof(Il2CppTLD.UI.MiniTopNav.Update))]
 
-        internal class JournalMissionTabDisplay
+        internal class MissionTabDisplay
         {
 
-            private static void Postfix(Panel_Log __instance)
+            public static void Prefix(Il2CppTLD.UI.MiniTopNav __instance)
             {
 
                 if (!Settings.settings.enabledMissionTab)
                 {
-
-                    GameObject navbarNormal = null;
-                    GameObject navbarTale = null;
-
-                    for (int i = 0; i < __instance.gameObject.transform.childCount; i++)
+                    foreach (var element in __instance.m_NavElements)
                     {
-                        GameObject child = __instance.gameObject.transform.GetChild(i).gameObject;
-
-                        if (child.name == "MiniTopNav")
+                        if (element.name == "SpriteMissions")
                         {
-                            navbarNormal = child;
-                        }
-                        else if (child.name == "MiniTopNavStory")
-                        {
-                            navbarTale = child;
-                        }
-                    }
-
-                    if (navbarNormal != null && navbarTale != null)
-                    {
-                        navbarTale.SetActive(false);
-                        navbarNormal.SetActive(true);
-                        navbarNormal.transform.GetChild(0).gameObject.SetActive(true);
-                    }
-                    else
-                    {
-                        MelonLogger.Msg("Can't find navbar objects in log");
-                    }
-
-
-                }
-            }
-
-        }
-
-        [HarmonyPatch(typeof(Panel_Inventory), nameof(Panel_Inventory.Update))]
-
-        internal class InventoryMissionTabDisplay
-        {
-            private static void Postfix(Panel_Inventory __instance)
-            {
-
-                if (!Settings.settings.enabledMissionTab)
-                {
-
-                    GameObject navbarNormal = null;
-                    GameObject navbarTale = null;
-
-                    for (int i = 0; i < __instance.gameObject.transform.childCount; i++)
-                    {
-                        GameObject child = __instance.gameObject.transform.GetChild(i).gameObject;
-
-                        if (child.name == "MiniTopNav")
-                        {
-                            navbarNormal = child;
-                        }
-                        else if (child.name == "MiniTopNavStory")
-                        {
-                            navbarTale = child;
-                        }
-                    }
-
-                    if (navbarNormal != null && navbarTale != null)
-                    {
-                        navbarTale.SetActive(false);
-                        navbarNormal.SetActive(true);
-                        navbarNormal.transform.GetChild(0).gameObject.SetActive(true);
-                    }
-                }
-            }
-        }
-
-        [HarmonyPatch(typeof(Panel_FirstAid), nameof(Panel_FirstAid.Update))]
-
-        internal class StatusMissionTabDisplay
-        {
-            private static void Postfix(Panel_FirstAid __instance)
-            {
-                if (!Settings.settings.enabledMissionTab)
-                {
-
-                    GameObject navbarNormal = null;
-                    GameObject navbarTale = null;
-
-                    for (int i = 0; i < __instance.gameObject.transform.childCount; i++)
-                    {
-                        GameObject child = __instance.gameObject.transform.GetChild(i).gameObject;
-
-                        if (child.name == "MiniTopNav")
-                        {
-                            navbarNormal = child;
-                        }
-                        else if (child.name == "MiniTopNavStory")
-                        {
-                            navbarTale = child;
-                        }
-                    }
-
-                    if (navbarNormal != null && navbarTale != null)
-                    {
-                        navbarTale.SetActive(false);
-                        navbarNormal.SetActive(true);
-                        navbarNormal.transform.GetChild(0).gameObject.SetActive(true);
+                            __instance.m_NavElements.Remove(element);
+                            GameObject child = __instance.gameObject.transform.Find("SpriteMissions").gameObject;
+                            child.SetActive(false);
+                            return;
+                        } 
                     }
                 }
             }
 
-        }
+        } 
 
-        [HarmonyPatch(typeof(Panel_Clothing), nameof(Panel_Clothing.Update))]
-
-        internal class ClothingMissionTabDisplay
-        {
-            private static void Postfix(Panel_Clothing __instance)
-            {
-
-                if (!Settings.settings.enabledMissionTab)
-                {
-
-                    GameObject nonPaperDoll = __instance.gameObject.transform.GetChild(1).gameObject;
-
-                    GameObject navbarNormal = null;
-                    GameObject navbarTale = null;
-
-                    for (int i = 0; i < nonPaperDoll.transform.childCount; i++)
-                    {
-                        GameObject child = nonPaperDoll.transform.GetChild(i).gameObject;
-
-                        if (child.name == "MiniTopNav")
-                        {
-                            navbarNormal = child;
-                        }
-                        else if (child.name == "MiniTopNavStory")
-                        {
-                            navbarTale = child;
-                        }
-                    }
-
-                    if (navbarNormal != null && navbarTale != null)
-                    {
-                        navbarTale.SetActive(false);
-                        navbarNormal.SetActive(true);
-                        navbarNormal.transform.GetChild(0).gameObject.SetActive(true);
-                    }
-                }
-
-            }
-
-        }
-
-        [HarmonyPatch(typeof(Panel_Crafting), nameof(Panel_Crafting.Update))]
-
-        internal class CraftingMissionTabDisplay
-        {
-            private static void Postfix(Panel_Crafting __instance)
-            {
-
-                if (!Settings.settings.enabledMissionTab)
-                {
-
-                    GameObject root = __instance.gameObject.transform.GetChild(0).gameObject;
-
-                    GameObject navbarNormal = null;
-                    GameObject navbarTale = null;
-
-                    for (int i = 0; i < root.transform.childCount; i++)
-                    {
-                        GameObject child = root.transform.GetChild(i).gameObject;
-
-                        if (child.name == "MiniTopNavSandbox")
-                        {
-                            navbarNormal = child;
-                        }
-                        else if (child.name == "MiniTopNavStory")
-                        {
-                            navbarTale = child;
-                        }
-                    }
-
-                    if (navbarNormal != null && navbarTale != null)
-                    {
-                        navbarTale.SetActive(false);
-                        navbarNormal.SetActive(true);
-                        navbarNormal.transform.GetChild(0).gameObject.SetActive(true);
-                    }
-                }
-
-            }
-
-        }
-
-        [HarmonyPatch(typeof(Panel_Map), nameof(Panel_Map.Update))]
-
-        internal class MapMissionTabDisplay
-        {
-            private static void Postfix(Panel_Map __instance)
-            {
-
-                if (!Settings.settings.enabledMissionTab)
-                {
-
-                    GameObject root = __instance.gameObject.transform.GetChild(0).gameObject;
-
-                    GameObject navbarNormal = null;
-                    GameObject navbarTale = null;
-
-                    for (int i = 0; i < root.transform.childCount; i++)
-                    {
-                        GameObject child = root.transform.GetChild(i).gameObject;
-
-                        if (child.name == "MiniTopNavSandbox")
-                        {
-                            navbarNormal = child;
-                        }
-                        else if (child.name == "MiniTopNavStory")
-                        {
-                            navbarTale = child;
-                        }
-                    }
-
-                    if (navbarNormal != null && navbarTale != null)
-                    {
-                        navbarTale.SetActive(false);
-                        navbarNormal.SetActive(true);
-                        navbarNormal.transform.GetChild(0).gameObject.SetActive(true);
-                    }
-                }
-
-            }
-
-        }
-
+       
         [HarmonyPatch(typeof(Panel_HUD), nameof(Panel_HUD.Update))]
         internal class HUDMissionPopupDisplay
         {
@@ -275,21 +61,50 @@ namespace ImprovedSignalVoid.Patches.Patches
             }
         }
 
-        [HarmonyPatch(typeof(Panel_Log), nameof(Panel_Log.EnterState))]
 
-        internal class MissionTabDeactivate
+        [HarmonyPatch(typeof(Panel_Log), nameof(Panel_Log.EnableFromRadial))]
+        internal class MissionTabDeactivateFromJournalOpen
         {
-            private static void Prefix(Panel_Log __instance, ref PanelLogState newState)
+            private static void Prefix(Panel_Log __instance, ref PanelLogState state)
             {
+
                 if (!Settings.settings.enabledMissionTab)
                 {
-                    if(newState == PanelLogState.Missions)
-                    {
-                        __instance.m_NavigationTabState = Panel_Log.NavigationTabState.Journal;
-                        newState = PanelLogState.DayListStats;
-                    }
+                    state = PanelLogState.DayListStats;
                 }
             }
+
+        }
+
+        [HarmonyPatch(typeof(Panel_Log), nameof(Panel_Log.EnableFromObjective))]
+
+        internal class MissionTabDeactivateFromPopup
+        {
+            private static bool Prefix(Panel_Log __instance)
+            {
+                return !Settings.settings.enabledMissionTab ? false : true;
+            }
+
+            private static void Postfix(Panel_Log __instance)
+            {
+
+                if (!Settings.settings.enabledMissionTab)
+                {
+
+                    __instance.m_NavigationTabState = NavigationTabState.Journal;
+                    __instance.m_Log = GameManager.GetLogComponent();
+                    __instance.EnterState(PanelLogState.DayListStats);
+                    __instance.Enable(enable: true);
+                    __instance.Refresh();
+                    __instance.UpdateNavigationButtons();
+                    if (__instance.ShouldEnterSectionNav())
+                    {
+                        __instance.EnterSectionNav();
+                    }
+                }
+
+            }
+
         }
     }
 }
