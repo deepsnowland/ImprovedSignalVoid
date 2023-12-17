00efc0c000d0a152b57f4737f4e4aa469eb058b1
Fuar11 <mcaa6556@gmail.com>
2023-12-16 21:26:03 -0500

    Added proper modcomponent

---
+++ Patches/TaleItemPatches.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Il2Cpp;
+using ImprovedSignalVoid;
+using MelonLoader;
+
+namespace ImprovedTales.Patches
+{
+    internal class TaleItemPatches
+    {
+
+        [HarmonyPatch(typeof(GearItem), nameof(GearItem.Awake))]
+
+        public class RemoveLimitationsOnTaleItems
+        {
+            public static void Postfix(GearItem __instance)
+            {
+
+              //nothing yet
+                
+            }
+
+        }
+
+
+    }
+}
